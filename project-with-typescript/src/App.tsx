41e191e67bb5532eba83c16f05f46526b9acd1ea
mohit <mohitkumar.cse19@chitkarauniversity.edu.in>
2023-01-02 22:33:40 +0530

    Jan 2, 2023

--- project-with-typescript/src/App.tsx
+++ project-with-typescript/src/App.tsx
@@ -1,12 +1,16 @@
 import React,{useState} from 'react';
 import './App.css';
 import { InputField } from './Component/InputField';
+import { Todo } from "./model/model";
+
+
 const App:React.FC=()=> {
   const [todo,setTodo]=useState<string>("");
+  const [todos,setTodos]=useState<Todo[]>([]);
   return (
     <div className="App">
      <h1>TASKIFY</h1>
-     <InputField todo={todo} setTodo={setTodo}/>
+     <InputField todo={todo} setTodo={setTodo} Todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
--- project-with-typescript/src/Component/InputField.tsx
+++ project-with-typescript/src/Component/InputField.tsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Todo } from "../model/model";
 
-
-interface Props{
-    todo:String;
-    setTodo:React.Dispatch<React.SetStateAction<string>>
+interface Props {
+  todo: String;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  Todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
-export const InputField :React.FC<Props>= ({todo,setTodo}) => {
+export const InputField: React.FC<Props> = ({
+  todo,
+  setTodo,
+  Todos,
+  setTodos,
+}) => {
+  const inputTodo = useRef<null | HTMLInputElement>(null);
+  const handleForm = (e: React.FormEvent): void => {
+    e.preventDefault();
+    // console.log(typeof inputTodo);
+    // console.log(typeof setTodo);
+    setTodo(inputTodo.current?.value as string);
+    console.log(todo);
 
-    const handleForm=():void=>{
-
-    }
-  return <div className="input_field">
-    <form onSubmit={handleForm}>
-    <input placeholder="Enter your task"/>
-
-    </form>
-  </div>;
+    setTodos([
+      ...Todos,
+      {
+        id: Date.now().toString(),
+        todo: todo,
+        isDone: false,
+      },
+    ]);
+  };
+  return (
+    <div className="input_field">
+      <form onSubmit={handleForm}>
+        <input placeholder="Enter your task" ref={inputTodo} />
+        <button type="submit">Go</button>
+      </form>
+    </div>
+  );
 };
